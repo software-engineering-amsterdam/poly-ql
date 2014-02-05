eda1e15c3a3bba04158deffae0ecff246318310e
Fabien Tesselaar <fabien.tesselaar@student.uva.nl>
2014-02-05 12:56:54 +0100

    Added run script

--- fabien/run.sh
+++ fabien/run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 pegjs grammar/grammar.pegjs parser/parser.js
+node test.js
--- fabien/test.js
+++ fabien/test.js
@@ -1,5 +1,5 @@
 // Test Node file for parsing input
 var parser = require("./parser/parser");
-var out = parser.parse("Testing:works");
+var out = parser.parse("Testing works");
 
 console.log(out);
