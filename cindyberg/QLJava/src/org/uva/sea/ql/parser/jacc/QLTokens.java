e7645edac1e7c6d775ab855149679016bddf5dca
Cindy Berghuizen <c.berghuizen@gmail.com>
2014-02-06 13:24:37 +0100

    Grammar

---
+++ cindyberg/QLJava/src/org/uva/sea/ql/parser/jacc/QLTokens.java
@@ -0,0 +1,39 @@
+// Output created by jacc on Thu Feb 06 13:02:13 CET 2014
+
+package org.uva.sea.ql.parser.jacc;
+
+interface QLTokens {
+    int ENDINPUT = 0;
+    int AND = 1;
+    int BOOLEAN = 2;
+    int DATE = 3;
+    int DECIMAL = 4;
+    int EQ = 5;
+    int FORM = 6;
+    int GEQ = 7;
+    int IDENT = 8;
+    int IDENTIFIER = 9;
+    int IF = 10;
+    int INT = 11;
+    int INTEGER = 12;
+    int LABEL = 13;
+    int LEQ = 14;
+    int MONEY = 15;
+    int NEQ = 16;
+    int OR = 17;
+    int UMIN = 18;
+    int UNOT = 19;
+    int UPLUS = 20;
+    int error = 21;
+    // '!' (code=33)
+    // '(' (code=40)
+    // ')' (code=41)
+    // '*' (code=42)
+    // '+' (code=43)
+    // '-' (code=45)
+    // '/' (code=47)
+    // '<' (code=60)
+    // '>' (code=62)
+    // '{' (code=123)
+    // '}' (code=125)
+}
