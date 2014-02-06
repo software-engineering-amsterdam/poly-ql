bc3a84def2c598ecb133b7280f0d4de779b29db0
Edwin Westerhoud <edwin.westerhoud@student.uva.nl>
2014-02-06 18:45:14 +0100

    Project setup with the start of a lexer and parser in F# (using FsYacc and YsLex)

---
+++ EdwinWesterhoud/SoftwConsQL/QL_Fsharp/QL_AST.fs
@@ -0,0 +1,13 @@
+﻿// (1) Define AST / syntax
+module QL_AST
+
+type Identifier = string
+type Label = string
+
+type qtype =
+    | Boolean
+    | String
+    | Integer
+
+type question =
+    | Question of Identifier * Label * qtype
