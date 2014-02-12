9fd3b7aa1e954169286399a13b4c7bd3ace832b9
xhorjus <xander@horjus-it.nl>
2014-02-12 12:54:44 +0100

    Started grammar

--- xhorjus/QL/src/Test.java
+++ xhorjus/QL/src/Test.java
@@ -1,25 +1,32 @@
+import java.io.FileInputStream;
 import java.io.IOException;
 
-import org.antlr.v4.runtime.*;
-import org.antlr.v4.runtime.tree.*;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
 
 /**
  * Test class to instantiate Test expression and do stuff.
  */
 public class Test {
 	public static void main(String[] args) throws IOException {
-	    ANTLRInputStream input = new ANTLRInputStream(System.in);
+		// Create input stream from in.txt
+		FileInputStream in = new FileInputStream("src/in.txt");
+	    ANTLRInputStream input = new ANTLRInputStream(in);
 	    
+	    // Create lexer from input
 	    TestLexer lexer = new TestLexer(input);
 	    
+	    // Create tokens from lexer
 	    CommonTokenStream tokens = new CommonTokenStream(lexer);
 	    
+	    // Create parser from tokens
 	    TestParser parser = new TestParser(tokens);
 	   
-	    ParseTree tree = parser.init();
+	    // Create tree from init rule
+	    ParseTree tree = parser.questionnare();
 	    
+	    // Print tree. Tada.
 	    System.out.println(tree.toStringTree(parser));
-	    
 	}
-
 }
