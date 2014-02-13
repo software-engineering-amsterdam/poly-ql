74d7051c056a0bac3b8ead794aad058a23d84563
Ivan Plantevin <ivan.p92@gmail.com>
2014-02-13 12:42:24 +0100

    Initial JUnit infrastructure with expression tests

---
+++ ivan/ql-project/src/net/iplantevin/antlr/QLVisitor.java
@@ -0,0 +1,146 @@
+package net.iplantevin.antlr;
+// Generated from QL.g4 by ANTLR 4.2
+import org.antlr.v4.runtime.misc.NotNull;
+import org.antlr.v4.runtime.tree.ParseTreeVisitor;
+
+/**
+ * This interface defines a complete generic visitor for a parse tree produced
+ * by {@link QLParser}.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+public interface QLVisitor<T> extends ParseTreeVisitor<T> {
+	/**
+	 * Visit a parse tree produced by {@link QLParser#ifElse}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitIfElse(@NotNull QLParser.IfElseContext ctx);
+
+	/**
+	 * Visit a parse tree produced by {@link QLParser#form}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitForm(@NotNull QLParser.FormContext ctx);
+
+	/**
+	 * Visit a parse tree produced by {@link QLParser#integer}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitInteger(@NotNull QLParser.IntegerContext ctx);
+
+	/**
+	 * Visit a parse tree produced by {@link QLParser#block}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitBlock(@NotNull QLParser.BlockContext ctx);
+
+	/**
+	 * Visit a parse tree produced by {@link QLParser#type}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitType(@NotNull QLParser.TypeContext ctx);
+
+	/**
+	 * Visit a parse tree produced by {@link QLParser#addition}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitAddition(@NotNull QLParser.AdditionContext ctx);
+
+	/**
+	 * Visit a parse tree produced by {@link QLParser#comparison}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitComparison(@NotNull QLParser.ComparisonContext ctx);
+
+	/**
+	 * Visit a parse tree produced by {@link QLParser#unary}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitUnary(@NotNull QLParser.UnaryContext ctx);
+
+	/**
+	 * Visit a parse tree produced by {@link QLParser#logical}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitLogical(@NotNull QLParser.LogicalContext ctx);
+
+	/**
+	 * Visit a parse tree produced by {@link QLParser#multiplication}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitMultiplication(@NotNull QLParser.MultiplicationContext ctx);
+
+	/**
+	 * Visit a parse tree produced by {@link QLParser#if}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitIf(@NotNull QLParser.IfContext ctx);
+
+	/**
+	 * Visit a parse tree produced by {@link QLParser#string}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitString(@NotNull QLParser.StringContext ctx);
+
+	/**
+	 * Visit a parse tree produced by {@link QLParser#blockStat}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitBlockStat(@NotNull QLParser.BlockStatContext ctx);
+
+	/**
+	 * Visit a parse tree produced by {@link QLParser#computation}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitComputation(@NotNull QLParser.ComputationContext ctx);
+
+	/**
+	 * Visit a parse tree produced by {@link QLParser#boolean}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitBoolean(@NotNull QLParser.BooleanContext ctx);
+
+	/**
+	 * Visit a parse tree produced by {@link QLParser#bool}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitBool(@NotNull QLParser.BoolContext ctx);
+
+	/**
+	 * Visit a parse tree produced by {@link QLParser#question}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitQuestion(@NotNull QLParser.QuestionContext ctx);
+
+	/**
+	 * Visit a parse tree produced by {@link QLParser#identifier}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitIdentifier(@NotNull QLParser.IdentifierContext ctx);
+
+	/**
+	 * Visit a parse tree produced by {@link QLParser#parantheses}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitParantheses(@NotNull QLParser.ParanthesesContext ctx);
+}
---
+++ ivan/ql-project/src/net/iplantevin/tests/GrammarTest.java
@@ -0,0 +1,65 @@
+package net.iplantevin.tests;
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ivan
+ * Date: 13-02-14
+ * Tests for antlr4 grammar
+ */
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import org.antlr.v4.runtime.*;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import net.iplantevin.antlr.*;
+
+import static java.util.Arrays.asList;
+
+public class GrammarTest {
+    private static String basePath = "/Users/Ivan/Documents/Master SE/Software Construction/poly-ql/ivan/ql-project/src/net/iplantevin/tests/inputs/";
+    private static String ext = ".txt";
+    private static String outExt = "_out.txt";
+
+    private QLParser getParser(String fileName) throws IOException {
+        InputStream is = System.in;
+        is = new FileInputStream(basePath + fileName + ext);
+
+        ANTLRInputStream input = new ANTLRInputStream(is);
+        QLLexer lexer = new QLLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+        QLParser parser = new QLParser(tokens);
+        return parser;
+    }
+
+    private String getExpected(String fileName) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(basePath + fileName + outExt)));
+    }
+
+    private String getMessage(String fileName) {
+        return "The parse tree for input file '" + fileName + ext +
+               "' does not match the expected tree:";
+    }
+
+    @Test
+    public void testCorrectExpressions() throws IOException {
+        QLParser parser;
+        String expected, actual, message;
+
+        // Holds all files to test for expression rule.
+        String[] files = new String[]{"expr1", "expr2", "expr3", "expr4"};
+
+        for( String file : files) {
+            parser = getParser(file);
+            expected = getExpected(file);
+            actual = parser.expr().toStringTree(parser);
+            Assert.assertEquals(getMessage(file), expected, actual);
+        }
+    }
+}
