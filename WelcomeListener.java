34a38cc6bc40f328fb3d202ba6b394d4d5740df8
Nisha Jacob <nisha.jacob6@gmail.com>
2014-02-13 12:02:37 +0100

    initial commit

---
+++ WelcomeListener.java
@@ -0,0 +1,20 @@
+// Generated from Welcome.g4 by ANTLR 4.2
+import org.antlr.v4.runtime.misc.NotNull;
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link WelcomeParser}.
+ */
+public interface WelcomeListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link WelcomeParser#r}.
+	 * @param ctx the parse tree
+	 */
+	void enterR(@NotNull WelcomeParser.RContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link WelcomeParser#r}.
+	 * @param ctx the parse tree
+	 */
+	void exitR(@NotNull WelcomeParser.RContext ctx);
+}
