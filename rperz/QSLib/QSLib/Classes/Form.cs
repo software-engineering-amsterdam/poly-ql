3b53bf9da50354fb5cf324441851485ecda625e0
unknown <ROBIN352@LPC-LR9HGYNP.internal.towerswatson.com>
2014-02-19 11:00:50 +0100

    Added the main objects

--- rperz/QSLib/QSLib/Classes/Form.cs
+++ rperz/QSLib/QSLib/Classes/Form.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using QSLib.Expressions;
 
 namespace QSLib.Classes
 {
     class Form
     {
+        private List<Expression> _expList;
+        public Form(List<Expression> expList)
+        {
+            this._expList = expList;
+        }
     }
 }
