908336ca7bd56067d36e9e8f8453f197db7fa355
Santiago Carrillo <sancarbar@gmail.com>
2014-02-19 14:24:10 +0100

    Labels added to expressions. files moved. git ignore edited to skip IntelliJ IDEA config files.

--- SantiagoCarrillo/q-language/src/edu/uva/softwarecons/main/Main.java
+++ SantiagoCarrillo/q-language/src/edu/uva/softwarecons/main/Main.java
@@ -4,23 +4,28 @@ import edu.uva.softwarecons.grammar.QuestionnaireLexer;
 import edu.uva.softwarecons.grammar.QuestionnaireParser;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
 
-public class Q {
+public class Main {
     public static void main(String[] args) throws Exception {
         String inputFile = null;
+
         if ( args.length>0 ) inputFile = args[0];
-        InputStream is = System.in;
-        if ( inputFile!=null ) is = new FileInputStream(inputFile);
-        ANTLRInputStream input = new ANTLRInputStream(is);
+        InputStream inputStream = System.in;
+        if ( inputFile!=null ) inputStream = new FileInputStream(inputFile);
+
+        ANTLRInputStream input = new ANTLRInputStream(inputStream);
         QuestionnaireLexer lexer = new QuestionnaireLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         QuestionnaireParser parser = new QuestionnaireParser(tokens);
         ParseTree tree = parser.questionnaire(); // parse
         System.out.println(tree.toStringTree(parser));// print LISP-style tree
 
-        //eval.visit(tree);
+        ParseTreeWalker walker = new ParseTreeWalker();
+        Evaluator evaluator = new Evaluator();
+        walker.walk(evaluator, tree);
     }
 }
