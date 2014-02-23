a413ee355e1d9e5d2c80eee9d114e5d3098d6d99
Omar007 <Omar.networkhosting@gmail.com>
2014-02-23 22:13:42 +0100

    Moved some files around.
    Created a new project for the Term Frequency problem using Object Calisthenics. (folder has a 'l' to many but to lazy to fix atm)
    
    Removed some unused code.

--- OmarPakker/ReadingAssignments/Term Frequency/Program.cs
+++ OmarPakker/ReadingAssignments/Term Frequency/Program.cs
@@ -9,34 +9,23 @@ namespace Term_Frequency
 	{
 		static void Main(string[] args)
 		{
-			StreamReader[] readers =
+			string[] inputs =
 			{
-				File.OpenText("./Input/input.txt"),
-				File.OpenText("./Input/pride-and-prejudice.txt"),
-				File.OpenText("./Input/test.txt")
+				File.OpenText("../../../Input/input.txt").ReadToEnd(),
+				File.OpenText("../../../Input/pride-and-prejudice.txt").ReadToEnd(),
+				File.OpenText("../../../Input/test.txt").ReadToEnd()
 			};
 
 			TermFrequency m = new TermFrequency();
 
-			foreach (StreamReader reader in readers)
+			foreach (string input in inputs)
 			{
-				IEnumerable<KeyValuePair<string, int>> entries = m.CountWordOccurences(reader, 25);
+				IEnumerable<KeyValuePair<string, int>> entries = m.CountWordOccurences(input, 25);
 				foreach (string entry in entries.Select(kv => kv.Key + " => " + kv.Value))
 				{
 					Console.WriteLine(entry);
 				}
-
-				Console.WriteLine();
-				reader.BaseStream.Position = 0;
-
-				IEnumerable<KeyValuePair<string, HashSet<int>>> pages = m.GetPagesForWords(reader, 25);
-				foreach (string entry in pages.Select(kv => kv.Key + " => " + String.Join(", ", kv.Value)))
-				{
-					Console.WriteLine(entry);
-				}
-
-				Console.ReadKey();
-				Console.WriteLine();
+                Console.WriteLine();
 			}
 
 			Console.ReadKey();
