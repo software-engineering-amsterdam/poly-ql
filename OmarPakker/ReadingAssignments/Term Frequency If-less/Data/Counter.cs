fde875efd8bd4ab7598dab759eb55d2d3eec8a95
Omar007 <Omar.networkhosting@gmail.com>
2014-02-24 23:14:56 +0100

    Suddenly realized I did some retarded chaining using the NullCounter. Don't even ask me why I did it...
    Moved code out of the NullCounter. It now actually is a proper NullCounter >.<
    StackOverflowException no longer present on pride-and-prejudice.txt. Larger files may still suffer though.

--- OmarPakker/ReadingAssignments/Term Frequency If-less/Data/Counter.cs
+++ OmarPakker/ReadingAssignments/Term Frequency If-less/Data/Counter.cs
@@ -4,13 +4,13 @@ namespace Term_Frequency_If_less.Data
 {
     public class Counter
     {
-        public virtual void Count(List<Counter> counters, List<string> words, List<string>.Enumerator wordStepper, Dictionary<string, int> result)
+        public virtual void Count(List<Counter> counters, List<string> words, List<string>.Enumerator wordStepper, Dictionary<string, int> results)
         {
             wordStepper.MoveNext();
-            result.Add(wordStepper.Current, words.RemoveAll(w => w.Equals(wordStepper.Current)));
+            results.Add(wordStepper.Current, words.RemoveAll(w => w.Equals(wordStepper.Current)));
 
             counters.RemoveAt(0);
-            counters[0].Count(counters, words, words.GetEnumerator(), result);
+            counters[0].Count(counters, words, words.GetEnumerator(), results);
         }
     }
 }
--- OmarPakker/ReadingAssignments/Term Frequency If-less/TermFrequency.cs
+++ OmarPakker/ReadingAssignments/Term Frequency If-less/TermFrequency.cs
@@ -22,7 +22,7 @@ namespace Term_Frequency_If_less
         private void ProcessWords(List<string> words)
         {
             RemoveStopWords(words, new HashSet<string>(File.OpenText("../../../Input/stop_words.txt").ReadToEnd().Split(',')));
-            CreateCounters(new List<Counter>(), words, new HashSet<string>(words));
+            SetupResultsDictionary(new Dictionary<string, int>(), words);
         }
 
         private void RemoveStopWords(List<string> words, HashSet<string> stopWords)
@@ -30,12 +30,39 @@ namespace Term_Frequency_If_less
             words.RemoveAll(w => stopWords.Contains(w));
         }
 
-        private void CreateCounters(List<Counter> counters, List<string> words, HashSet<string> uniqueWords)
+        private void SetupResultsDictionary(Dictionary<string, int> results, List<string> words)
+        {
+            CreateAndRunCounters(new List<Counter>(), words, new HashSet<string>(words), results);
+            CreateAndRunPrinters(new List<KeyValuePair<string, int>>(results));
+        }
+
+        private void CreateAndRunCounters(List<Counter> counters, List<string> words, HashSet<string> uniqueWords, Dictionary<string, int> results)
         {
             new List<string>(uniqueWords).ForEach(w => counters.Add(new Counter()));
 
             counters.Add(new NullCounter());
-            counters[0].Count(counters, words, words.GetEnumerator(), new Dictionary<string, int>());
+            counters[0].Count(counters, words, words.GetEnumerator(), results);
+        }
+
+        private void CreateAndRunPrinters(List<KeyValuePair<string, int>> resultsList)
+        {
+            resultsList.Sort((kv1, kv2) => kv2.Value - kv1.Value);
+
+            CapOutput(new List<Printer>(), new Printer[Math.Min(resultsList.Count, 25)], resultsList);
+        }
+
+        private void CapOutput(List<Printer> printers, Printer[] limitedOutput, List<KeyValuePair<string, int>> resultsList)
+        {
+            resultsList.ForEach(w => printers.Add(new Printer()));
+            printers.CopyTo(0, limitedOutput, 0, Math.Min(printers.Count, 25));
+
+            RunPrinters(new List<Printer>(limitedOutput), resultsList);
+        }
+
+        private void RunPrinters(List<Printer> printers, List<KeyValuePair<string, int>> resultsList)
+        {
+            printers.Add(new NullPrinter());
+            printers[0].Print(printers, resultsList.GetEnumerator());
         }
 	}
 }
