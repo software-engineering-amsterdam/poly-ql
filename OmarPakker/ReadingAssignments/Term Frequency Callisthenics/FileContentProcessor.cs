fe7019478a3939511a388a9e6412ad6f7ba5e090
Omar007 <Omar.networkhosting@gmail.com>
2014-02-24 19:12:26 +0100

    I believe this satisfies every rule from Object Calisthenics. (Ofc I could  have overlooked something).

---
+++ OmarPakker/ReadingAssignments/Term Frequency Callisthenics/FileContentProcessor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Term_Frequency_Callisthenics.Data;
+
+namespace Term_Frequency_Callisthenics
+{
+    public class FileContentProcessor
+    {
+        private readonly List<Word> _words;
+
+        public FileContentProcessor(string fileContent)
+        {
+            _words = new List<Word>();
+
+            FilteredWords filteredWords = new FilteredWords(fileContent);
+            StripStopWords(filteredWords);
+        }
+
+        private void StripStopWords(IEnumerable<Word> words)
+        {
+            IEnumerator<Word> enumerator = words.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                HandleWord(enumerator.Current);
+            }
+        }
+
+        private void HandleWord(Word word)
+        {
+            bool isStopWord = StopWordManager.IsStopWord(word);
+            bool lengthGreaterThanOne = word.IsLongerThanOne();
+
+            if (!isStopWord && lengthGreaterThanOne)
+            {
+                _words.Add(word);
+            }
+        }
+
+        public IEnumerable<KeyValuePair<Word, OccurenceCount>> ProcessWords()
+        {
+            OccurencesManager occurencesManager = new OccurencesManager();
+            return occurencesManager.GetWordOccurences(_words);
+        }
+    }
+}
