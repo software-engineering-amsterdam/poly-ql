9b9ea9c27acec4d79d58ca480923009e81b69ead
Omar007 <Omar.networkhosting@gmail.com>
2014-02-24 19:12:26 +0100

    I believe this satisfies every rule from Object Calisthenics. (Ofc I could  have overlooked something).

---
+++ OmarPakker/ReadingAssignments/Term Frequency Callisthenics/OccurencesManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Term_Frequency_Callisthenics.Data;
+
+namespace Term_Frequency_Callisthenics
+{
+    public class OccurencesManager
+    {
+        private Dictionary<Word, OccurenceCount> _occurences;
+
+        public OccurencesManager()
+        {
+            _occurences = new Dictionary<Word, OccurenceCount>();
+        }
+
+        private OccurenceCount SortKeySelector(KeyValuePair<Word, OccurenceCount> keyValuePair)
+        {
+            return keyValuePair.Value;
+        }
+
+        private void ProcessWord(Word word)
+        {
+            if (!_occurences.ContainsKey(word))
+            {
+                _occurences.Add(word, new OccurenceCount());
+            }
+            _occurences[word].Increment();
+        }
+
+        public IEnumerable<KeyValuePair<Word, OccurenceCount>> GetWordOccurences(IEnumerable<Word> words)
+        {
+            foreach (Word word in words)
+            {
+                ProcessWord(word);
+            }
+
+            return _occurences.OrderByDescending(SortKeySelector);
+        }
+    }
+}
--- OmarPakker/ReadingAssignments/Term Frequency Callisthenics/Program.cs
+++ OmarPakker/ReadingAssignments/Term Frequency Callisthenics/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Term_Frequency_Callisthenics.Data;
 
 namespace Term_Frequency_Callisthenics
 {
@@ -10,7 +10,20 @@ namespace Term_Frequency_Callisthenics
     {
         static void Main(string[] args)
         {
-            TermFrequency m = new TermFrequency();
+            StreamReader inputFileStream = File.OpenText("../../../Input/pride-and-prejudice.txt");
+            string fileContent = inputFileStream.ReadToEnd();
+
+            FileContentProcessor processor = new FileContentProcessor(fileContent);
+            IEnumerable<KeyValuePair<Word, OccurenceCount>> occurences = processor.ProcessWords();
+
+            foreach (KeyValuePair<Word, OccurenceCount> occurence in occurences.Take(25))
+            {
+                Word word = occurence.Key;
+                OccurenceCount count = occurence.Value;
+                Console.WriteLine(word + " => " + count);
+            }
+
+            Console.ReadKey();
         }
     }
 }
