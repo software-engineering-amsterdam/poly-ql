6d09bad28931eec494884dda8dce620fe476c855
Omar007 <Omar.networkhosting@gmail.com>
2014-02-25 13:40:49 +0100

    Realized a stack is a better idea to use in the recursion.

--- OmarPakker/ReadingAssignments/Term Frequency If-less/TermFrequency.cs
+++ OmarPakker/ReadingAssignments/Term Frequency If-less/TermFrequency.cs
@@ -32,37 +32,38 @@ namespace Term_Frequency_If_less
 
         private void SetupResultsDictionary(Dictionary<string, int> results, List<string> words)
         {
-            CreateAndRunCounters(new List<Counter>(), words, new HashSet<string>(words), results);
+            CreateAndRunCounters(new Stack<Counter>(), words, new HashSet<string>(words), results);
             CreateAndRunPrinters(new List<KeyValuePair<string, int>>(results));
         }
 
-        private void CreateAndRunCounters(List<Counter> counters, List<string> words, HashSet<string> uniqueWords, Dictionary<string, int> results)
+        private void CreateAndRunCounters(Stack<Counter> counters, List<string> words, HashSet<string> uniqueWords, Dictionary<string, int> results)
         {
-            new List<string>(uniqueWords).ForEach(w => counters.Add(new Counter()));
+            counters.Push(new NullCounter());
+            new List<string>(uniqueWords).ForEach(w => counters.Push(new Counter()));
 
-            counters.Add(new NullCounter());
-            counters[0].Count(counters, words, words.GetEnumerator(), results);
+            counters.Pop().Count(counters, words, results);
         }
 
         private void CreateAndRunPrinters(List<KeyValuePair<string, int>> resultsList)
         {
             resultsList.Sort((kv1, kv2) => kv2.Value - kv1.Value);
 
-            CapOutput(new List<Printer>(), new Printer[Math.Min(resultsList.Count, 25)], resultsList);
+            CapOutput(new List<Printer>(), new Printer[Math.Min(resultsList.Count + 1, 26)], resultsList);
         }
 
         private void CapOutput(List<Printer> printers, Printer[] limitedOutput, List<KeyValuePair<string, int>> resultsList)
         {
+            printers.Add(new NullPrinter());
             resultsList.ForEach(w => printers.Add(new Printer()));
-            printers.CopyTo(0, limitedOutput, 0, Math.Min(printers.Count, 25));
 
-            RunPrinters(new List<Printer>(limitedOutput), resultsList);
+            printers.CopyTo(0, limitedOutput, 0, Math.Min(printers.Count, 26));
+
+            RunPrinters(new Stack<Printer>(limitedOutput), resultsList);
         }
 
-        private void RunPrinters(List<Printer> printers, List<KeyValuePair<string, int>> resultsList)
+        private void RunPrinters(Stack<Printer> printers, List<KeyValuePair<string, int>> resultsList)
         {
-            printers.Add(new NullPrinter());
-            printers[0].Print(printers, resultsList.GetEnumerator());
+            printers.Pop().Print(printers, resultsList.GetEnumerator());
         }
     }
 }
