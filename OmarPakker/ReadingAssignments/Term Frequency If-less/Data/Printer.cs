2e1364c4554666437f0c79bc68b762fcd972384a
Omar007 <Omar.networkhosting@gmail.com>
2014-02-25 13:40:49 +0100

    Realized a stack is a better idea to use in the recursion.

--- OmarPakker/ReadingAssignments/Term Frequency If-less/Data/Printer.cs
+++ OmarPakker/ReadingAssignments/Term Frequency If-less/Data/Printer.cs
@@ -5,13 +5,12 @@ namespace Term_Frequency_If_less.Data
 {
     public class Printer
     {
-        public virtual void Print(List<Printer> printers, List<KeyValuePair<string, int>>.Enumerator occurenceStepper)
+        public virtual void Print(Stack<Printer> printers, List<KeyValuePair<string, int>>.Enumerator occurenceStepper)
         {
             occurenceStepper.MoveNext();
             Console.WriteLine(occurenceStepper.Current.Key + " => " + occurenceStepper.Current.Value);
 
-            printers.RemoveAt(0);
-            printers[0].Print(printers, occurenceStepper);
+            printers.Pop().Print(printers, occurenceStepper);
         }
     }
 }
