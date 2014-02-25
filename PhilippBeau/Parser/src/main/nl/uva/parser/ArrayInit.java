4dc52ab7f94db57abc79087eda17ae68f50662c6
Philipp Beau <PhilippB3au@gmail.com>
2014-02-25 13:36:11 +0100

    Antlr init

---
+++ PhilippBeau/Parser/src/main/nl/uva/parser/ArrayInit.java
@@ -0,0 +1,37 @@
+package main.nl.uva.parser;
+
+/***
+ * Excerpted from "The Definitive ANTLR 4 Reference",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material, 
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose. 
+ * Visit http://www.pragmaticprogrammer.com/titles/tpantlr2 for more book information.
+ ***/
+// import ANTLR's runtime libraries
+import main.nl.uva.g4.ArrayInitLexer;
+import main.nl.uva.g4.ArrayInitParser;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+public class ArrayInit {
+    public static void main(final String[] args) throws Exception {
+
+        // create a CharStream that reads from standard input
+        ANTLRInputStream input = new ANTLRInputStream("{1,2,3}");
+
+        // create a lexer that feeds off of input CharStream
+        ArrayInitLexer lexer = new ArrayInitLexer(input);
+
+        // create a buffer of tokens pulled from the lexer
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+        // create a parser that feeds off the tokens buffer
+        ArrayInitParser parser = new ArrayInitParser(tokens);
+
+        ParseTree tree = parser.init(); // begin parsing at init rule
+        System.out.println(tree.toStringTree(parser)); // print LISP-style tree
+    }
+}
