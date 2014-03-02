16249ed15864b49108a9eba5e30e5384807a789c
samkatt <sammie.katt@gmail.com>
2014-03-02 17:19:16 +0100

    Now actually added the files >.>

---
+++ skatt/QL/src/org/uva/sea/ql/parser/antlr/QL4/AST/IQLTree.java
@@ -0,0 +1,11 @@
+package org.uva.sea.ql.parser.antlr.QL4.AST;
+
+/**
+ * Interface for abstract syntax trees for QL4. Every class 
+ * created during parsing extends this
+ * @author Sammie Katt
+ *
+ */
+public abstract class IQLTree {
+
+}
---
+++ skatt/QL/src/org/uva/sea/ql/parser/antlr/QL4/AST/QL4Form.java
@@ -0,0 +1,25 @@
+package org.uva.sea.ql.parser.antlr.QL4.AST;
+
+import java.util.List;
+
+/**
+ * Abstract syntax tree of the form construct in the QL4 grammar
+ * @author Sammie Katt
+ *
+ */
+public class QL4Form extends IQLTree {
+	
+	/**
+	 * The structures of the form, the upper most 
+	 * collection of all questions and conditionals
+	 */
+	IQLTree structures;
+	
+	/**
+	 * sets structures of the form 
+	 * @param structures is the structures tree
+	 */
+	public void setStructures(IQLTree structures) {
+		this.structures = structures;
+	}
+}
