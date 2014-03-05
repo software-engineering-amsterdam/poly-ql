ddcca3fdb43820979c2e9feae84bb17edfcdf3c2
samkatt <sammie.katt@gmail.com>
2014-03-05 17:38:02 +0100

    Changed value extension from tree to expression

--- skatt/QL/src/org/uva/sea/ql/parser/antlr/QL4/AST/Value/Value.java
+++ skatt/QL/src/org/uva/sea/ql/parser/antlr/QL4/AST/Value/Value.java
@@ -1,7 +1,7 @@
 package org.uva.sea.ql.parser.antlr.QL4.AST.Value;
 
-import org.uva.sea.ql.parser.antlr.QL4.AST.QLTree;
+import org.uva.sea.ql.parser.antlr.QL4.AST.Expression.Expression;
 
-public abstract class Value extends QLTree {
+public abstract class Value extends Expression {
 
 }
