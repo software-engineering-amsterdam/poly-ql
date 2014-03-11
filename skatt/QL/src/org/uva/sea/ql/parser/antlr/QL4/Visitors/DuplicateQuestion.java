20b813bb4c4f79b1f4d0f8c76df63e0a0c3f3f48
samkatt <sammie.katt@gmail.com>
2014-03-11 22:26:31 +0100

    Wrote duplicate identifier visitor

---
+++ skatt/QL/src/org/uva/sea/ql/parser/antlr/QL4/Visitors/DuplicateQuestion.java
@@ -0,0 +1,41 @@
+package org.uva.sea.ql.parser.antlr.QL4.Visitors;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.uva.sea.ql.parser.antlr.QL4.AST.Question;
+import org.uva.sea.ql.parser.antlr.QL4.AST.Value.Identifier;
+import org.uva.sea.ql.parser.antlr.QL4.TypeChecker.QLErrorMsg;
+
+/**
+ * Visitor that checks a QLTree on duplicate question identifiers
+ * @author Sammie Katt
+ *
+ */
+public class DuplicateQuestion extends QLErrorVisitor {
+
+	/**
+	 * The list of identifiers found when visiting
+	 */
+	List<Identifier> ids = new ArrayList<Identifier>();
+	
+	@Override
+	/**
+	 * When visiting a question, its identifier is compared to 
+	 * previous visited questions identifiers. If it is a duplicate,
+	 * a warning is returned, otherwise it is added to the list 
+	 * of known identifiers 
+	 */
+	public List<QLErrorMsg> visit(Question question) {
+		List<QLErrorMsg> msgs = new ArrayList<QLErrorMsg>();
+		Identifier qId = question.getIdentifier();
+		
+		if (ids.contains(qId)) {
+			msgs.add(new QLErrorMsg("A duplicate question id is used: " + qId));
+		} else {
+			ids.add(qId);
+		}
+		
+		return msgs;
+	}
+}
