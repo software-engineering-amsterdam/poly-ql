0a7807b01505c281f4bc12e1ac1f53e87fba35b8
samkatt <sammie.katt@gmail.com>
2014-03-11 23:30:07 +0100

    Wrote the binary visiting methods in invalidType

--- skatt/QL/src/org/uva/sea/ql/parser/antlr/QL4/Visitors/InvalidType.java
+++ skatt/QL/src/org/uva/sea/ql/parser/antlr/QL4/Visitors/InvalidType.java
@@ -1,8 +1,8 @@
 package org.uva.sea.ql.parser.antlr.QL4.Visitors;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import org.uva.sea.ql.parser.antlr.QL4.AST.Expression.BinaryExpr;
 import org.uva.sea.ql.parser.antlr.QL4.AST.Expression.Expression;
 import org.uva.sea.ql.parser.antlr.QL4.AST.Expression.UnaryExpr;
 import org.uva.sea.ql.parser.antlr.QL4.AST.Expression.Binary.BiLogicExpr;
@@ -18,17 +18,63 @@ import org.uva.sea.ql.parser.antlr.QL4.TypeChecker.QLErrorMsg;
 /**
  * Checks whether the type of expressions in operators are
  * appropriate, otherwise returns a (list of) error(s).
+ * 
+ * Note: Of the binary operations, it will not override the default
+ * visiting behavior of the equalityExpr. Equality can be tested
+ * between different types, as it will simply return false. 
+ * 
  * @author Sammie Katt
  *
  */
 public class InvalidType extends QLErrorVisitor {
 
+	/**
+	 * TODO
+	 */
+	@Override
 	public List<QLErrorMsg> visit(UnaryExpr expr) {
+		System.err.println("Unary Expression not yet implemented");
 		return null;
 	}
 	
-	public List<QLErrorMsg> visit(BinaryExpr expr) {
-		return null;
+	/**
+	 * If visiting a mathematical expression, check whether
+	 * both its lhs and rhs are mathematical.
+	 */
+	@Override
+	public List<QLErrorMsg> visit(BiMathExpr expr) {
+		List<QLErrorMsg> msgs = new ArrayList<QLErrorMsg>();
+		
+		// generate warning if not both hand sides are mathematical
+		if (! (isMathematical(expr.getLHS()) && isMathematical(expr.getRHS()) ) ) {
+			msgs.add(new QLErrorMsg("Cannot perform " + expr));
+		}
+		
+		// add errors and warnings from childs
+		msgs.addAll(visitChild(expr.getLHS()));
+		msgs.addAll(visitChild(expr.getRHS()));
+		
+		return msgs;
+	}
+	
+	/**
+	 * If visiting a logical expression, check whether
+	 * both its lhs and rhs are logical.
+	 */
+	@Override
+	public List<QLErrorMsg> visit(BiLogicExpr expr) {
+		List<QLErrorMsg> msgs = new ArrayList<QLErrorMsg>();
+		
+		// generate warning if not both hand sides are logical
+		if (! (isLogical(expr.getLHS()) && isLogical(expr.getRHS()) ) ) {
+			msgs.add(new QLErrorMsg("Cannot perform " + expr));
+		}
+		
+		// add errors and warnings from childs
+		msgs.addAll(visitChild(expr.getLHS()));
+		msgs.addAll(visitChild(expr.getRHS()));
+		
+		return msgs;
 	}
 	
 	/**
