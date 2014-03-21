7f4202d51dc3e154ef863ce5d00552147787efae
samkatt <sammie.katt@gmail.com>
2014-03-21 11:45:56 +0100

    Actually added moved files now

---
+++ skatt/QL/src/org/uva/sea/ql/parser/antlr/QL4/AST/Label.java
@@ -0,0 +1,49 @@
+package org.uva.sea.ql.parser.antlr.QL4.AST;
+
+import org.uva.sea.ql.parser.antlr.QL4.Visitors.IQLVisitor;
+
+/**
+ * Represents QL AST label node
+ * @author Sammie Katt
+ *
+ */
+public class Label implements QLTree {
+	private final String value;
+	
+	public Label(String value) {
+		this.value = value;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Label other = (Label) obj;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
+	}
+
+	public Object accept(IQLVisitor<?> visitor) {
+		return visitor.visit(this);
+	}
+	
+	public String toString() {
+		return value;
+	}
+}
